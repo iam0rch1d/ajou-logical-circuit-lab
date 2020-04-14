4f2269af7603012d24b7df53fccef02737fec86e
iam0rch1d <heavyknell@gmail.com>
2020-04-15 00:39:49 +0900

    Add semicolon at encoderDecimalToExcess3.v

--- experiment5/sources/encoderDecimalToExcess3.v
+++ experiment5/sources/encoderDecimalToExcess3.v
@@ -6,6 +6,6 @@ module encoderDecimalToExcess3 (
     assign out[0] = in[0] | in[2] | in[4];
     assign out[1] = in[0] | in[3] | in[4];
     assign out[2] = in[1] | in[2] | in[3] | in[4];
-    assign out[3] = in[5]
+    assign out[3] = in[5];
 
 endmodule
