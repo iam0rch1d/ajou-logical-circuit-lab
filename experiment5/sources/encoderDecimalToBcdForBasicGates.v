4cd2b94ab257a6805500108058a7fe942cd403b9
iam0rch1d <heavyknell@gmail.com>
2020-04-15 02:34:54 +0900

    Fix logical error at encoderDecimalToBcdForBasicGates.v

--- experiment5/sources/encoderDecimalToBcdForBasicGates.v
+++ experiment5/sources/encoderDecimalToBcdForBasicGates.v
@@ -6,7 +6,7 @@ module encoderDecimalToBcdForBasicGates (
     // Active-high
     assign out[0] = in[1] | in[3] | in[5] | in[7] | in[9];
     assign out[1] = in[2] | in[3] | in[6] | in[7];
-    assign out[2] = in[4] | in[5] | in[7];
+    assign out[2] = in[4] | in[5] | in[6] | in[7];
     assign out[3] = in[8] | in[9];
 
 endmodule
