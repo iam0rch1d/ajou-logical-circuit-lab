0840df6d40e1a4251714bb3f36bfd867d0806d4f
iam0rch1d <heavyknell@gmail.com>
2020-04-21 06:04:04 +0900

    Describe latchSrWithEnableByNorGate by using NOR gate only

--- experiment6/sources/latchSrWithEnableByNorGate.v
+++ experiment6/sources/latchSrWithEnableByNorGate.v
@@ -10,11 +10,11 @@ module latchSrWithEnableByNorGate (
     reg reg_outputFirstStage1;
     
     always@ (enable, set, reset) begin
-        reg_outputFirstStage0 = ~(enable | set);
-        reg_outputFirstStage1 = ~(enable | reset);
+        reg_outputFirstStage0 = ~(enable | reset);
+        reg_outputFirstStage1 = ~(enable | set);
         
-        out = ~(notout & reg_outputFirstStage0);
-        notout = ~(out & reg_outputFirstStage1);
+        out = ~(notout | reg_outputFirstStage0);
+        notout = ~(out | reg_outputFirstStage1);
     end
 
 endmodule
