e8acd59e639c4c9b04bd550f33caca2c57f9ee10
iam0rch1d <heavyknell@gmail.com>
2020-04-29 20:39:38 +0900

    Replace the function of enable with preset in flip-flops

--- experiment6/sources/flipflopD.v
+++ experiment6/sources/flipflopD.v
@@ -7,11 +7,16 @@ module flipflopD (
     output reg notout
     );
 
-    always@ (posedge clockpulse, posedge clear) begin
+    always@ (posedge clockpulse, posedge clear, posedge preset) begin
         if (clear) begin
             out <= 1'b0;
+            notout <= 1'b1;
         end
         else if (preset) begin
+            out <= 1'b1;
+            notout <= 1'b0;
+        end
+        else begin
             out <= data;
             notout <= ~data;
         end
--- experiment6/sources/flipflopJk.v
+++ experiment6/sources/flipflopJk.v
@@ -8,11 +8,16 @@ module flipflopJk (
     output reg notout
     );
 
-    always@ (posedge clockpulse, posedge clear) begin
+    always@ (posedge clockpulse, posedge preset, posedge clear) begin
         if (clear) begin
             out <= 1'b0;
+            notout <= 1'b1;
         end
         else if (preset) begin
+            out <= 1'b1;
+            notout <= 1'b0;
+        end
+        else begin
             if (~jack & kilby) begin
                 out <= 1'b0;
                 notout <= 1'b1;
